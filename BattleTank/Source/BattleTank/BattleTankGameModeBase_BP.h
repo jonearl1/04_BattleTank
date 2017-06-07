aed6b7a407d008aeb3f401c5f19a25ebdd1a4ad8
jonearl-pc\Jon <jon.r.earl@gmail.com>
2017-06-07 15:28:56 +0100

    Vid112

---
+++ BattleTank/Source/BattleTank/BattleTankGameModeBase_BP.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BattleTankGameModeBase.h"
+#include "BattleTankGameModeBase_BP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameModeBase_BP : public ABattleTankGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
